7da3dbbf7d82e5f94dcbcf606310ca1d78f350a5
Fabio.TH <fabio.azevedo@sptech.school>
2025-09-04 18:10:28 -0300

    Realizando PopUp de Confirmação

--- front-end-m.c-plastico/src/pages/login-de-usuario/login-de-usuario.jsx
+++ front-end-m.c-plastico/src/pages/login-de-usuario/login-de-usuario.jsx
@@ -1,6 +1,7 @@
 import './css/login-de-usuario.css'
 import LogoMC from '../../assets/img/logo/McLogo.svg'
 import LoginForm from './components/login-form/LoginForm'
+import PopupCheck from '../../components/popup-check/popup-check';
 
 function LoginDeUsuario() {
     return (
@@ -10,6 +11,7 @@ function LoginDeUsuario() {
             </div>
             <h1>Bem-vindo(a) de volta!</h1>
             <LoginForm />
+            <PopupCheck text="Login realizado com sucesso!" />
         </div>
     );
 }
