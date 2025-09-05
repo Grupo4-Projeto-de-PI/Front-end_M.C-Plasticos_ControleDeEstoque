82ad024aac93d274852647811dde04ef0c9182f6
Lucas Canuto <lucas.canuto@sptech.school>
2025-09-05 19:43:27 -0300

    Bug de autocomplete arrumado

--- front-end-m.c-plastico/src/main.jsx
+++ front-end-m.c-plastico/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import LoginDeUsuario from './pages/login-de-usuario/view/login-de-usuario'
+import LoginDeUsuario from './pages/login-de-usuario/view/login-de-usuario.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
