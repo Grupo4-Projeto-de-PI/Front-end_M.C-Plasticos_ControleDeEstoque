138a050edc1a9e0f8f02337525de300bff09d6a2
Fabio.TH <fabio.azevedo@sptech.school>
2025-09-08 18:19:13 -0300

    .

--- front-end-m.c-plastico/src/main.jsx
+++ front-end-m.c-plastico/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import LoginDeUsuario from './pages/login-de-usuario/view/login-de-usuario'
+import ListarEstoqueAtual from './pages/listar-estoque-atual/view/listar-estoque-atual'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LoginDeUsuario />
+    <ListarEstoqueAtual />
   </StrictMode>,
 )
