18293f17224493ee080f9d8c189ad7f54dedf5c3
Lucas Canuto <lucas.canuto@sptech.school>
2025-09-16 23:26:55 -0300

    Adicionando controller listar-estoque-atual

---
+++ front-end-m.c-plastico/src/pages/Jornada_de_Estoque_Atual/listar-estoque-atual/controller/listar-estoque-atual.controller.jsx
@@ -0,0 +1,9 @@
+import ListarEstoqueAtual from "../view/listar-estoque-atual";
+
+function ListarEstoqueAtualController() {
+    return (
+        <ListarEstoqueAtual />
+    )
+}
+
+export default ListarEstoqueAtualController;
