473b0ce4d21ce659f38ca48e9716bbd6e75c8565
Lucas Canuto <lucas.canuto@sptech.school>
2025-09-16 23:28:11 -0300

    removendo controller duplicada

--- front-end-m.c-plastico/src/pages/controller/listar-estoque-atual.controller.jsx
+++
@@ -1,9 +0,0 @@
-import ListarEstoqueAtual from "../view/listar-estoque-atual";
-
-function ListarEstoqueAtualController() {
-    return (
-        <ListarEstoqueAtual />
-    )
-}
-
-export default ListarEstoqueAtualController;
