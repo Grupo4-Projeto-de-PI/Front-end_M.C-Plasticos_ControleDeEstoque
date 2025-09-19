b68440265723a4f35e9f78e8b16b13b4910e1535
FaberaTH <fabio.azevedo@sptech.school>
2025-09-19 20:18:38 -0300

    Componentizando tela de listar produtos do estoque atual

--- front-end-m.c-plastico/src/routes.jsx
+++ front-end-m.c-plastico/src/routes.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import { LoginUsuarioController } from "./pages/Jornada_Tela_Inicial/login-de-usuario/index.js";
 import { ListarHistoricoController } from "./pages/Jornada_De_Transacoes/listar-historico/index.js";
 import { ListarEstoqueAtualController } from "./pages/Jornada_de_Estoque_Atual/listar-estoque-atual/index.js";
 import { CriarProdutoController } from "./pages/Jornada_de_Produto/criar-produto/index.js";
@@ -7,7 +6,7 @@ import { CriarProdutoController } from "./pages/Jornada_de_Produto/criar-produto
 function AppRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<LoginUsuarioController />} />
+            <Route path="/" element={<ListarEstoqueAtualController />} />
             <Route path="/historico-transacao" element={<ListarHistoricoController />} />
             <Route path="/listar-estoque-atual" element={<ListarEstoqueAtualController />} />
             <Route path="/criar-produto" element={<CriarProdutoController />} />
