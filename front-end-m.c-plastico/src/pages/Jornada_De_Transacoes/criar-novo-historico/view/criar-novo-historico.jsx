eca747330e6d05e896f1aae2116615c99fd0e783
Lucas Canuto <lucas.canuto@sptech.school>
2025-09-24 11:11:20 -0300

    removendo componente pai de formulario

--- front-end-m.c-plastico/src/pages/Jornada_De_Transacoes/criar-novo-historico/view/criar-novo-historico.jsx
+++ front-end-m.c-plastico/src/pages/Jornada_De_Transacoes/criar-novo-historico/view/criar-novo-historico.jsx
@@ -1,17 +1,26 @@
 import Header2 from "@components/header-2/header-2";
 import Footer from "@components/footer/footer";
 import Button from "@components/button/button";
-import FormNovoHistorico from "../components/form-novo-historico/form-novo-historico";
+import FormCardNovoHistorico from '../components/form-card-novo-historico/card-form-novo-historico';
+import FormCardSelectNovoHistorico from '../components/form-card-select-novo-historico/form-card-select-novo-historico';
 import "../css/criar-novo-historico.css";
 
 
 function CriarNovoHistorico() {
     return (
         <>
-            <Header2 text={'Criar Novo Histórico'}/>
+            <Header2 text={'Criar Novo Histórico'} />
             <main className="conteudo">
-                <FormNovoHistorico title={'Crie as informações de histórico'}/>
-                <Button text={'Cadastrar Novo Histórico'}/>
+                <div className="card">
+                    <h2>Crie as informações de histórico</h2>
+                    <FormCardSelectNovoHistorico title={'Produto'} textOption={'Selecione um produto'} idOption={1} />
+                    <FormCardNovoHistorico title={'Quantidade (kg)'} placeholder={'Insira o peso do produto'} />
+                    <FormCardNovoHistorico title={'Preço (R$)'} placeholder={'Insira o preço do produto'} />
+                    <FormCardSelectNovoHistorico title={'Tipo da Operação'} textOption={'Selecione o tipo da operação'} idOption={1} />
+                    <FormCardSelectNovoHistorico title={'Categoria'} textOption={'Seleciona a categoria'} idOption={1} />
+                    <FormCardSelectNovoHistorico title={'Parceiro Comercial'} textOption={'Selecione o parceiro comercial'} idOption={1} />
+                </div>
+                <Button text={'Cadastrar Novo Histórico'} />
             </main>
             <Footer />
         </>
