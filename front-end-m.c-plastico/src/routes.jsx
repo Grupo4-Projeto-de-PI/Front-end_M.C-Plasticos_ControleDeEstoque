c33e7f97f41745cafca66fb6d66979b507f14bc5
Lucas Canuto <lucas.canuto@sptech.school>
2025-09-24 20:24:19 -0300

    arrumando o import das routes

--- front-end-m.c-plastico/src/routes.jsx
+++ front-end-m.c-plastico/src/routes.jsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import { ListarHistoricoController } from "./pages/Jornada_De_Transacoes/listar-historico/index.js";
-import { ListarEstoqueAtualController } from "./pages/Jornada_de_Estoque_Atual/listar-estoque-atual/index.js";
-import { CriarProdutoController } from "./pages/Jornada_de_Produto/criar-produto/index.js";
-import { CriarNovoHistoricoController } from "./pages/Jornada_De_Transacoes/criar-novo-historico/index.js";
-import { ListarInformacoesParceiro } from "./pages/jornada_de_Parceiro_Comercial/listar-informacao-parceiro/index.js";
+import { ListarHistoricoController } from "@pages/Jornada_De_Transacoes/listar-historico/index.js";
+import { ListarEstoqueAtualController } from "@pages/Jornada_de_Estoque_Atual/listar-estoque-atual/index.js";
+import { CriarProdutoController } from "@pages/Jornada_de_Produto/criar-produto/index.js";
+import { CriarNovoHistoricoController } from "@pages/Jornada_De_Transacoes/criar-novo-historico/index.js";
+import { ListarInformacoesParceiro } from "@pages/jornada_de_Parceiro_Comercial/listar-informacao-parceiro/index.js";
+import { LoginUsuarioController } from "@pages/Jornada_Tela_Inicial/login-de-usuario/index.js";
 function AppRoutes() {
 
     return (
