52a07ddebab548f0d3051570b8b9461a01aefe40
joaojp06 <joao.antonio@sptech.school>
2025-09-28 23:43:42 -0300

    Componentes da tela de informações do produto (estoque atual)

---
+++ front-end-m.c-plastico/src/pages/Jornada_de_Estoque_Atual/listar-estoque-atual/components/card-info-produto/card-info-produto.jsx
@@ -0,0 +1,10 @@
+import './card-info-produtos.css'
+
+export default function Card({ title, children }) {
+    return (
+      <div className="card">
+        {title && <h2>{title}</h2>}
+        {children}
+      </div>
+    );
+  }
