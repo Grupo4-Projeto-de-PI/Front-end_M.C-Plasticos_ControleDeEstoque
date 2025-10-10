240a233916f4c9e687da01fcfae4ee7d10e0b9cc
Lucas Canuto <lucas.canuto@sptech.school>
2025-10-10 19:51:56 -0300

    arrumando o header 2

--- front-end-m.c-plastico/src/pages/Jornada_De_Transacoes/selecao-filtros/index.js
+++ front-end-m.c-plastico/src/pages/Jornada_De_Transacoes/selecao-filtros/index.js
@@ -2,10 +2,14 @@ import FiltroOperacaoController from "./controller/filtro-operacao.controller";
 import FiltroTipoMaterialController from "./controller/filtro-tipo-material.controller";
 import FiltroTipoCategoria from "./controller/filtro-categoria";
 import FiltroTipoParceiroComercial from "./controller/filtro-tipo-parceiro";
+import FiltroFornecedor from "./controller/filtro-fornecedor";
+import FiltroCliente from "./controller/filtro-cliente";
 
 export { 
     FiltroOperacaoController, 
     FiltroTipoMaterialController, 
     FiltroTipoCategoria, 
-    FiltroTipoParceiroComercial 
+    FiltroTipoParceiroComercial,
+    FiltroFornecedor,
+    FiltroCliente
 };
--- front-end-m.c-plastico/src/routes.jsx
+++ front-end-m.c-plastico/src/routes.jsx
@@ -6,7 +6,12 @@ import { CriarNovoHistoricoController } from "@pages/Jornada_De_Transacoes/criar
 import { ListarInformacoesParceiro } from "@pages/Jornada_De_Parceiro_Comercial/listar-informacao-parceiro/index";
 import { LoginUsuarioController } from "@pages/Jornada_Tela_Inicial/login-de-usuario/index.js";
 import { FiltroHistoricoTransacaoController } from "@pages/Jornada_De_Transacoes/filtro-transacoes/index.js";
-import { FiltroOperacaoController, FiltroTipoMaterialController, FiltroTipoCategoria } from "@pages/Jornada_De_Transacoes/selecao-filtros/index.js";
+import {
+    FiltroOperacaoController,
+    FiltroTipoMaterialController,
+    FiltroTipoCategoria,
+    FiltroTipoParceiroComercial
+} from "@pages/Jornada_De_Transacoes/selecao-filtros/index.js";
 function AppRoutes() {
 
     return (
@@ -18,6 +23,7 @@ function AppRoutes() {
             <Route path="/criar-novo-historico" element={<CriarNovoHistoricoController />} />
             <Route path="/listar-informacoes-parceiro" element={<ListarInformacoesParceiro />} />
             <Route path="/filtro-historico-transacao" element={<FiltroHistoricoTransacaoController />} />
+            <Route path="/filtro-tipo-parceiro" element={<FiltroTipoParceiroComercial />} />
             <Route path="/filtro-operacao" element={<FiltroOperacaoController />} />
             <Route path="/filtro-tipo-material" element={<FiltroTipoMaterialController />} />
             <Route path="/filtro-tipo-categoria" element={<FiltroTipoCategoria />} />
