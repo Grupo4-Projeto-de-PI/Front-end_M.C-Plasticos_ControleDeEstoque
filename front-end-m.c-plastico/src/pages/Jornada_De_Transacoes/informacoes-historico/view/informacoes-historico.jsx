0e5bb7ca5ba6793f777a97f7e3a0a00475645b9d
Lucas Canuto <lucas.canuto@sptech.school>
2025-10-25 12:47:59 -0300

    arrumando css da pagina

--- front-end-m.c-plastico/src/pages/Jornada_De_Transacoes/informacoes-historico/view/informacoes-historico.jsx
+++ front-end-m.c-plastico/src/pages/Jornada_De_Transacoes/informacoes-historico/view/informacoes-historico.jsx
@@ -2,20 +2,19 @@ import Footer from '../../../../components/footer/footer';
 import Header2 from '../../../../components/header-2/header-2';
 import CardLog from '../components/card-log/card-log';
 import CardInfoHistoricoTransacao from '../components/card-info/card-info';
-import iconPencil from '../../../../assets/icons/icon-pencil.svg';
 import '../css/informacoes-historico.css';
 
 function InformacoesHistorico({ handleBackClick, historicoData }) {
   return (
-    <>
-      <Header2 text="Log do Histórico" onClickBack={() => handleBackClick()} pencil={true} />
+      <div className="pagina-informacoes-historico">
+        <Header2 text="Log do Histórico" onClickBack={() => handleBackClick()} pencil={true} />
         <main className="conteudo-informacao-historico">
           <CardLog logData={historicoData} />
           <h2>Informações do Histórico</h2>
           <CardInfoHistoricoTransacao historicoData={historicoData} />
         </main>
-      <Footer />
-    </>
+        <Footer />
+      </div>
   );
 }
 
