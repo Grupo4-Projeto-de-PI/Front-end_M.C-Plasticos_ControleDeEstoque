71ec92662c24991dfc821003ad530a1d9c0f6ace
Lucas Canuto <lucas.canuto@sptech.school>
2025-11-14 11:24:48 -0300

    criando estrutura inicial da jornada de python

---
+++ front-end-m.c-plastico/src/pages/Jornada_De_Transacoes/leitor-planilha-excel/controller/leitor-planilha-excel.controller.jsx
@@ -0,0 +1,9 @@
+import LeitorPlanilhaExcel from "../view/leitor-planilha-excel";
+
+function LeitorPlanilhaExcelController(){
+    return (
+        <LeitorPlanilhaExcel />
+    )
+}
+
+export default LeitorPlanilhaExcelController;
---
+++ front-end-m.c-plastico/src/pages/Jornada_De_Transacoes/leitor-planilha-excel/view/leitor-planilha-excel.jsx
@@ -0,0 +1,7 @@
+function LeitorPlanilhaExcel(){
+    return (
+        <div>Leitor Planilha Excel View</div>
+    )
+}
+
+export default LeitorPlanilhaExcel;
