5247eca429c5d1f913f0b2620fa8d77c8de6d222
fabriciogomes86820605 <fabricio.bonacelli@sptech.school>
2025-11-16 20:42:27 -0300

    limite de arquivo imagens

--- front-end-m.c-plastico/src/routes.jsx
+++ front-end-m.c-plastico/src/routes.jsx
@@ -11,7 +11,7 @@ import { ListarProdutosController } from "@pages/Jornada_de_Produto/listar-produ
 import { ListarFuncionarioController } from "@pages/Jornada_Tela_de_Administrador/listar-funcionario";
 import { CriarTipoMaterialController } from "@pages/Jornada_de_Produto/criar-tipo-material";
 import { InformacoesProdutoController } from "@pages/Jornada_de_Produto/informacoes-produto/index";
-import { ListarParceirosController } from "@pages/Jornada_De_Parceiro_Comercial/listar-parceiros/index.js";
+import { ListarParceirosController } from "@pages/Jornada_De_Parceiro_Comercial/listar-parceiros/index";
 import { EditarHierarquiaController } from "./pages/Jornada_Tela_de_Administrador/editar-hierarquia/index.js";
 import { CadastroParceiroComercialController } from "@pages/Jornada_De_Parceiro_Comercial/cadastro-parceiro-comercial/index";
 import { InformacoesHistoricoController } from "@pages/Jornada_De_Transacoes/informacoes-historico/index";
